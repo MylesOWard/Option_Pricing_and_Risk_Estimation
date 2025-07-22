bf04b3e5dcf32549b12fdaa10dbac5f5ff070be9
Myles Ward <mw@yellowdave.co.uk>
2025-07-22 20:47:51 +0100

    Update b_Heston_Model.py

--- b_Heston_Model.py
+++ b_Heston_Model.py
@@ -55,20 +55,47 @@ def heston_model(S, v0, rho, kappa, theta, sigma, T, N, M):
     # reassign price and variances varaibles into arrays
     # these arrays store prices and variances
     S = np.full(shape = (N+1,M), fill_value = S)
-    V = np.full(shape = (N+1,M), fill_value = V)
+    V = np.full(shape = (N+1,M), fill_value = v0)
 
     # sampling correlated brownian motion
-    Z = np.random.multivariate_normal(mu, cov (N,M))
+    # gives a full matrix of results for all time steps and simulations
+    Z = np.random.multivariate_normal(mu, cov, (N,M))
 
     # first we can compute changes in stock price based using the previous volatility value
     # this is a time-addative version of the ds equation, employing Ito's Lemma 
-    for i in range (1,N-1):
+    for i in range (1,N+1):
         # using logorithms to ensure calculations are time-addative
-        S[i] = S[i-1] * np.exp((r - 0.5*V[i-1])*dt + np.sqrt(V[i-1]*dt)*Z[i-1:,0])
+        S[i] = S[i-1] * np.exp((r - 0.5*V[i-1])*dt + np.sqrt(V[i-1]*dt)*Z[i-1,:,0])
         # using Euler discretisation 
         V[i] = np.maximum(V[i-1] + kappa*(theta-V[i-1])*dt + sigma*np.sqrt(V[i-1]*dt)*Z[i-1,:,1],0)
 
-    return S,v
+    return S,V
+
+rho_p = 0.98
+rho_n = -0.98
+
+S_p, V_p = heston_model(S, v0, rho_p, kappa, theta, sigma, T, N, M)
+S_n, V_n = heston_model(S, v0, rho_n, kappa, theta, sigma, T, N, M)
+
+# plotting
+fig, (ax1, ax2) = plt.subplots(1,2,figsize=(10,5))
+time = np.linspace(0,T,N+1)
+
+# plotting variation in stock price over time 
+ax1.plot(time,S_p)
+ax1.set_title("Heston Model Stock Prices")
+ax1.set_xlabel("Time")
+ax1.set_ylabel("Asset Prices")
+
+# plotting the variance over time
+ax2.plot(time,V_p)
+ax2.set_title("Heston Model Variance Over Time")
+ax2.set_xlabel("Time")
+ax2.set_ylabel("Variance")
+
+# since colours aren't specified they are applied in order
+# therefore outlier stock price paths will be colour-coded with outlier variance
+plt.show()
 
 
     
